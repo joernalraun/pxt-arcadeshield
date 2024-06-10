25bc8187f546fdf4701ee41fbd696ee3e9b0f0f3
Tom Ball <tball@microsoft.com>
2024-06-10 10:03:27 -0700

    bring more files over from micro*

---
+++ ui/cursorscene.ts
@@ -0,0 +1,171 @@
+namespace ui {
+    // TODO remove this
+    export enum CursorSceneEnum {
+        LiveDataViewer,
+        SensorSelect,
+        MeasurementConfigSelect,
+        RecordData,
+    }
+    
+    export class CursorScene extends Scene {
+        navigator: INavigator
+        public cursor: Cursor
+        public picker: Picker
+
+        constructor(app: App, navigator?: INavigator) {
+            super(app, "scene")
+            this.backgroundColor = 11
+            this.navigator = navigator
+        }
+
+        protected moveCursor(dir: CursorDir) {
+            try {
+                this.moveTo(this.cursor.move(dir))
+            } catch (e) {
+                if (dir === CursorDir.Up && e.kind === BACK_BUTTON_ERROR_KIND)
+                    this.back()
+                else if (
+                    dir === CursorDir.Down &&
+                    e.kind === FORWARD_BUTTON_ERROR_KIND
+                )
+                    return
+                else throw e 
+            }
+        }
+
+        protected moveTo(target: Button) {
+            if (!target) return
+            this.cursor.moveTo(
+                target.xfrm.worldPos,
+                target.ariaId,
+                target.bounds
+            )
+        }
+
+        /* override */ startup() {
+            super.startup()
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.right.id,
+                () => this.moveCursor(CursorDir.Right)
+            )
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.up.id,
+                () => this.moveCursor(CursorDir.Up)
+            )
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.down.id,
+                () => this.moveCursor(CursorDir.Down)
+            )
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.left.id,
+                () => this.moveCursor(CursorDir.Left)
+            )
+
+            // click
+            const click = () => this.cursor.click()
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.A.id,
+                click
+            )
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.A.id + keymap.PLAYER_OFFSET,
+                click
+            )
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.B.id,
+                () => this.back()
+            )
+
+            this.cursor = new Cursor()
+            this.picker = new Picker(this.cursor)
+            this.navigator = new RowNavigator()
+            this.cursor.navigator = this.navigator
+        }
+
+        back() {
+            if (!this.cursor.cancel()) this.moveCursor(CursorDir.Back)
+        }
+
+        protected handleClick(x: number, y: number) {
+            const target = this.cursor.navigator.screenToButton(
+                x - Screen.HALF_WIDTH,
+                y - Screen.HALF_HEIGHT
+            )
+            if (target) {
+                this.moveTo(target)
+                target.click()
+            } else if (this.picker.visible) {
+                this.picker.hide()
+            }
+        }
+
+        protected handleMove(x: number, y: number) {
+            const btn = this.cursor.navigator.screenToButton(
+                x - Screen.HALF_WIDTH,
+                y - Screen.HALF_HEIGHT
+            )
+            if (btn) {
+                const w = btn.xfrm.worldPos
+                this.cursor.snapTo(w.x, w.y, btn.ariaId, btn.bounds)
+                btn.reportAria(true)
+            }
+        }
+
+        /* override */ shutdown() {
+            this.navigator.clear()
+        }
+
+        /* override */ activate() {
+            super.activate()
+            const btn = this.navigator.initialCursor(0, 0)
+            if (btn) {
+                const w = btn.xfrm.worldPos
+                this.cursor.snapTo(w.x, w.y, btn.ariaId, btn.bounds)
+                btn.reportAria(true)
+            }
+        }
+
+        /* override */ update() {
+            this.cursor.update()
+        }
+
+        /* override */ draw() {
+            this.picker.draw()
+            this.cursor.draw()
+        }
+    }
+
+    
+    export class CursorSceneWithPriorPage extends CursorScene {
+        private goBack1PageFn: () => void;
+
+        constructor(app: App, goBack1PageFn: () => void) {
+            super(app)
+            this.backgroundColor = 11
+
+            this.goBack1PageFn = goBack1PageFn
+        }
+
+        /* override */ startup() {
+            super.startup()
+        
+            control.onEvent(
+                ControllerButtonEvent.Pressed,
+                controller.B.id,
+                () => this.goBack1PageFn()
+            )
+
+            this.cursor = new Cursor()
+            this.picker = new Picker(this.cursor)
+            this.navigator = new RowNavigator()
+            this.cursor.navigator = this.navigator
+        }
+    }
+}
